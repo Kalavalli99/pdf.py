770c351e1eb3f0c572cd2f16f7efc64bed9fcdd4
Kalavalli99 <75485779+Kalavalli99@users.noreply.github.com>
2021-03-25 12:59:19 +0530

    Update merge.py

--- merge.py
+++ merge.py
@@ -1,29 +1,18 @@
-# importing required modules
-import PyPDF2
-  
-def PDFmerge(pdfs, output): 
-    # creating pdf file merger object
-    pdfMerger = PyPDF2.PdfFileMerger()
-      
-    # appending pdfs one by one
-    for pdf in pdfs:
-        with open(pdf, 'rb') as f:
-            pdfMerger.append(f)
-          
-    # writing combined pdf to output pdf file
-    with open(output, 'wb') as f:
-        pdfMerger.write(f)
-  
-def main():
-    # pdf files to merge
-    pdfs = ['example.pdf', 'rotated_example.pdf']
-     
-    # output pdf file name
-    output  = 'combined_example.pdf'
-      
-    # calling pdf merge function
-    PDFmerge(pdfs = pdfs, output = output)
-  
-if __name__ == "__main__":
-    # calling the main function
-    main()
+from PyPDF2 import PdfFileReader, PdfFileWriter
+
+def merge_pdfs(paths, output):
+    pdf_writer = PdfFileWriter()
+
+    for path in paths:
+        pdf_reader = PdfFileReader(path)
+        for page in range(pdf_reader.getNumPages()):
+            # Add each page to the writer object
+            pdf_writer.addPage(pdf_reader.getPage(page))
+
+    # Write out the merged PDF
+    with open(output, 'wb') as out:
+        pdf_writer.write(out)
+
+if __name__ == '__main__':
+    paths = ['1.pdf', '2.pdf']
+    merge_pdfs(paths, output='merged1.pdf')
